33381003d89c8d304e821995d8d1cae8a89e2cd4
OockCuan <st10439216@imconnect.edu.za>
2025-09-17 18:38:29 +0200

    Finalization of UI
    Finalization of all UI elements, including their margins and distances between each other

--- obj/Debug/net9.0/ProgPOE.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProgPOE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgPOE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e5b6c737cb391c75673cc198db3058b7f8a66af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf69f81e7aaf53446184a226463763a8c00a17b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgPOE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgPOE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
