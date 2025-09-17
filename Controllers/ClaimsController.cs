6de36cd5bb1f723aebba8eee25960e81f82927f5
OockCuan <st10439216@imconnect.edu.za>
2025-09-17 16:04:40 +0200

    Initial Commit
    Addition of some basic files for the project and editing of the home view

---
+++ Controllers/ClaimsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgPOE.Controllers
+{
+    public class ClaimsController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
